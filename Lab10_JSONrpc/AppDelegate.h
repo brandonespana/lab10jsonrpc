d9d91672d3940d5a52a324e0535f595b9b2dc264
biespana <biespana@cntr150macg02.care.asu.edu>
2015-04-06 11:23:35 -0700

    first

---
+++ Lab10_JSONrpc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lab10_JSONrpc
+//
+//  Created by biespana on 4/6/15.
+//  Copyright (c) 2015 biespana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
